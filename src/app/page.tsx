b6632e85a84646a0af48bcfa2966a10f6227a2a6
Aseem Ratha <aseemratha@gmail.com>
2023-06-17 22:36:27 -0400

    ui work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,10 @@ import { Header } from '../components/header';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className=" glow-in p-2 rounded-xl border border-co2 bg-co1 m-3 flex flex-col items-center justify-between">
       <Header className='flex' />
-      <div className=''>
-          <div className=''>
+      <div className='flex flex-row'>
+          <div className='flex flex-col'>
               <Info_window />
               <AI className='' />
           </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'co1': '#000000',
+        'co2': '#EBCD9C',
+        'co3': '#3D00E0',
+      },
     },
   },
   plugins: [],
