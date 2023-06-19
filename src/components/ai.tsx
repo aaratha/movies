a2f7432e3906c58c630e188cbd1b724d4686bd94
aaratha <101074206+aaratha@users.noreply.github.com>
2023-06-19 16:38:16 -0400

    updates

--- src/components/ai.tsx
+++ src/components/ai.tsx
@@ -12,11 +12,11 @@ export const AI = ({ className }: AIProps) => {
     return (
         <div className=' w-2/5 p-[2px] bg-gradient-to-r from-co2 to-grad rounded-lg ml-4 mt-4 mr-4'>
             <div className=' flex flex-col h-full  rounded-lg'>
-                <div className='justify-between nestflex flex-row pl-4 pr-4 p-1 rounded-xl items-center'>
-                    <h1 className='text-black font-bold'>AI:</h1>
-                    <button className=' hover:scale-90 transition-all text-black border-black ml-3 p-1 pl-2 pr-2 rounded-full border-2'>Convincing</button>
-                    <button className='ml-3 p-1 pl-2 pr-2 hover:scale-90 rounded-full border-2 text-black border-black transition-all'>Academic</button>
-                    <button className='ml-3 p-1 pl-2 pr-2 hover:scale-90 rounded-full border-2 text-black border-black transition-all'>Background</button>
+                <div className='justify-between nestflex flex flex-row pl-4 pr-4 p-1 rounded-xl items-center'>
+                    <h1 className='ml-3 text-black font-bold'>AI:</h1>
+                    <button className='scale-90 hover:scale-[80%] transition-all text-black border-black ml-1 p-1 pl-2 pr-2 rounded-full border-2'>Convincing</button>
+                    <button className='ml-1 p-1 pl-2 pr-2 scale-90 hover:scale-[80%] rounded-full border-2 text-black border-black transition-all'>Academic</button>
+                    <button className='ml-1 mr-3 p-1 pl-2 pr-2 scale-90 hover:scale-[80%] rounded-full border-2 text-black border-black transition-all'>Background</button>
                 </div>
                 <div className=' mt-[2px] p-4 h-full bg-co1 rounded-xl'>
                     <p>
