1a45ebe54d8123a1a0fe3e97aefe86cbb3b8b632
Aseem Ratha <aseemratha@gmail.com>
2023-06-22 15:34:06 -0400

    info header

--- src/components/info-window.tsx
+++ src/components/info-window.tsx
@@ -21,7 +21,7 @@ export const Info_window = ({ className }: Info_windowProps) => {
     return (
         <div className='w-[42.86%] p-[2px] bg-gradient-to-r from-co2 to-grad rounded-lg mt-4'>
             <div className=' flex flex-col h-full p-4 bg-co1 rounded-lg'>
-                <h1 className=''>The Hourglass Sanitorium</h1>
+                <h1 className='bg-gradient-to-r from-co2 to-grad text-black rounded-lg p-2 pl-4 text-center mb-4 text-lg'>The Hourglass Sanitorium</h1>
                 <p>1973 ‘Sanatorium pod klepsydrą’ Directed by Wojciech Has</p>
                 <p>
                     A young man named Josef visits a dilapidated Sanatorium to see his father Jakob. On
