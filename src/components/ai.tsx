34e50cdc31323ec746c4793533ebed70698f1644
Aseem Ratha <aseemratha@gmail.com>
2023-06-22 15:02:15 -0400

    reformatted ai options

--- src/components/ai.tsx
+++ src/components/ai.tsx
@@ -13,10 +13,12 @@ export const AI = ({ className }: AIProps) => {
         <div className=' w-[42.86%] p-[2px] bg-gradient-to-r from-co2 to-grad rounded-lg ml-4 mt-4 mr-4'>
             <div className=' flex flex-col h-full  rounded-lg'>
                 <div className='justify-between nestflex flex flex-row pl-4 pr-4 p-1 rounded-xl items-center'>
-                    <h1 className='ml-3 text-black font-bold'>AI:</h1>
-                    <button className='but2 scale-93 hover:pl-3 hover:pr-3 transition-all text-black border-black ml-1 p-1 pl-2 pr-2 rounded-full border-2 hover:border-grad'>Convincing</button>
-                    <button className='but2 ml-1 p-1 pl-2 pr-2 scale-93 hover:pl-3 hover:pr-3 rounded-full border-2 text-black border-black hover:border-grad'>Academic</button>
-                    <button className='but2 ml-1 mr-3 p-1 pl-2 pr-2 scale-93 hover:pl-3 hover:pr-3 rounded-full border-2 text-black border-black hover:border-grad'>Background</button>
+                    <h1 className='ml-1 text-black font-bold'>AI:</h1>
+                    <div>
+                        <button className='but2 scale-93 hover:pl-3 hover:pr-3 transition-all text-black border-black ml-1 p-1 pl-2 pr-2 rounded-full border-2 hover:border-grad'>Convincing</button>
+                        <button className='but2 ml-2 p-1 pl-2 pr-2 scale-93 hover:pl-3 hover:pr-3 rounded-full border-2 text-black border-black hover:border-grad'>Academic</button>
+                        <button className='but2 ml-2 mr-1 p-1 pl-2 pr-2 scale-93 hover:pl-3 hover:pr-3 rounded-full border-2 text-black border-black hover:border-grad'>Background</button>
+                    </div>
                 </div>
                 <div className=' mt-[2px] p-4 h-full bg-co1 rounded-lg'>
                     <p>
