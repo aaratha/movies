1654956f47a820c7bea21a196301795bf2ee6c2a
aaratha <101074206+aaratha@users.noreply.github.com>
2023-07-07 13:25:07 -0400

    submit svg

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -55,7 +55,18 @@ export const Header: React.FC<HeaderProps> = ({ onSearch, className }) => {
                                 </div>
                             )}
                         </div>
-                        <button type='submit' className='but w-20 bg-gradient-to-r from-co2 to-grad rounded-full p-1 pl-2 pr-2 transition-all hover:scale-95 hover:bg-yellow'>Submit</button>
+                        <button type='submit' className='w-9 h-9 but bg-gradient-to-r from-co2 to-grad rounded-full p-1 pl-2 pr-2 transition-all hover:scale-95 hover:bg-yellow'>
+                            <svg className='' fill="#000000" height="20px" width="20px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xmlSpace="preserve">
+                                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                                <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+                                <g id="SVGRepo_iconCarrier">
+                                    <g>
+                                        <path d="M480,0H32C14.328,0,0,14.328,0,32v64c0,17.672,14.328,32,32,32s32-14.328,32-32V64h384v384H64v-32 c0-17.672-14.328-32-32-32S0,398.328,0,416v64c0,17.672,14.328,32,32,32h448c17.672,0,32-14.328,32-32V32 C512,14.328,497.672,0,480,0z"></path>
+                                        <path d="M240.906,348.211C245.625,350.742,250.828,352,256,352c6.203,0,12.391-1.805,17.75-5.375l96-64 C378.656,276.688,384,266.703,384,256s-5.344-20.688-14.25-26.625l-96-64c-9.828-6.555-22.484-7.156-32.844-1.586 C230.5,169.352,224,180.195,224,192v32H32c-17.672,0-32,14.328-32,32s14.328,32,32,32h192v32 C224,331.805,230.5,342.648,240.906,348.211z"></path>
+                                    </g>
+                                </g>
+                            </svg>
+                        </button>
                     </form>
                     {isInputFocused && (
                         <ul>
