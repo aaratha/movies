15cbd91c3448b3bb1860ad6e36c304d2f3ca19c1
aaratha <101074206+aaratha@users.noreply.github.com>
2023-07-09 21:20:13 -0400

    buttons

--- src/components/ai.tsx
+++ src/components/ai.tsx
@@ -14,7 +14,7 @@ export const AI = ({ className }: AIProps) => {
             <div className='f flex flex-col h-full  rounded-lg'>
                 <div className=' justify-center sm:justify-between flex flex-row flex-wrap pl-1 pr-1 p-1 md:pl-4 md:pr-4 rounded-xl items-center '>
                     <h1 className='ml-1 text-black font-bold text-center items-center content-center'>AI</h1>
-                    <div className='w-full sm:w-auto text-center'>
+                    <div className='w-full sm:w-auto text-center flex justify-between pr-2 pl-2'>
                         <button className=' z-10 but2 scale-x-100 sm:scale-x-100 scale-93 hover:pl-3 hover:pr-3 transition-all text-black hover:text-white p-1 rounded-full'>Background</button>
                         <button className='text-black hover:text-white z-10 but2 scale-x-100 sm:scale-x-100 ml-2 sm:ml-3 p-1 pl-2 pr-2 scale-93 rounded-full border-black hover:border-opacity-0'>Reception</button>
                         <button className='text-black hover:text-white z-10 but2 scale-x-100 sm:scale-x-100 ml-2 sm:ml-3 p-1 pl-2 pr-2 scale-93 rounded-full border-black hover:border-opacity-0'>Impact</button>
