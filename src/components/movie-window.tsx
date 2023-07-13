0662648ce00c22e44dcfaeaf2fe3159276e0591a
aaratha <101074206+aaratha@users.noreply.github.com>
2023-07-13 16:34:14 -0400

    moviewindow

--- src/components/movie-window.tsx
+++ src/components/movie-window.tsx
@@ -13,9 +13,9 @@ export interface Movie_windowProps {
  */
 export const Movie_window = ({ className }: Movie_windowProps) => {
     return (
-        <div className='relative lg:w-[14.28%] p-[0px] bg-gradient-to-r from-co2 to-grad rounded-lg mt-3 flex h-full flex-col'>
+        <div className='relative lg:w-[14.28%] p-[2px] bg-gradient-to-r from-co2 to-grad rounded-lg mt-3 flex h-full flex-col'>
             <h1 className='ml-1 p-3 text-center text-black font-bold'>Similar Movies</h1>
-            <div className=' relative scrollbar-none flex overflow-scroll lg:h-full flex-row lg:flex-col p-4 bg-gradient-to-b from-co1 to-co12  rounded-none rounded-b-lg lg:w-auto'>
+            <div className=' relative scrollbar-none flex overflow-scroll lg:h-full flex-row lg:flex-col p-4 rounded-lg lg:w-auto bg-bg2 bg-opacity-90'>
                 <Image
                     src={img}
                     className=' lg:w-[90%] mr-4 lg:mr-auto lg:h-[90%] w-28 h-28 but m-auto mb-4 mt-3 hover:scale-[94%] rounded-lg hover:rounded-none'
@@ -42,7 +42,8 @@ export const Movie_window = ({ className }: Movie_windowProps) => {
                     alt="The Hourglass Sanatorium movie poster"
                 />
             </div>
-            <div className=' lg:h-[10vh] w-full absolute bottom-0 left-0 bg-gradient-to-t from-co12 rounded-b-lg to-transparent'></div>
+            <div className=' lg:h-[10vh] w-[95.5%] absolute bottom-0.5 left-1 bg-gradient-to-t from-bg2 rounded-b-lg to-transparent'></div>
+            
         </div>
     );
 };
